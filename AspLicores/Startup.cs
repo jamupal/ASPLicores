05024c00cc6e124d1a37f10c3342ad14dafcef70
Hamilton Murillo Palacio <hamupal@hotmail.com>
2016-05-20 08:11:46 -0500

    Agregar archivos de proyecto.

---
+++ AspLicores/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspLicores.Startup))]
+namespace AspLicores
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
